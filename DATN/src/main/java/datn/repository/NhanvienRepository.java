ac8ab428f70b61912f148ff486c64953edb2c2d5
Nguyennamdsa123 <namn26848@gmail.com>
2025-06-23 21:32:46 +0700

    NhanVien

---
+++ DATN/src/main/java/datn/repository/NhanvienRepository.java
@@ -0,0 +1,7 @@
+package datn.repository;
+
+import com.example.nhanvien.Model.nhan_vien;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NhanvienRepository extends JpaRepository<nhan_vien,Integer> {
+}
